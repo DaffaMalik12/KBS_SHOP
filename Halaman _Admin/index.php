e938fbd741be575401909db22d06a868d1d05b63
DaffaMalik12 <dmalikakram@gmail.com>
2023-11-07 20:02:05 +0700

    membuat login system

--- Halaman _Admin/index.php
+++ Halaman _Admin/index.php
@@ -8,7 +8,21 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <title>Admin Page</title>
 </head>
+<?php
+    session_start();
 
+    // cek apakah yang mengakses halaman ini sudah login
+    if ($_SESSION['level'] == "") {
+        header("location:login.php?pesan=gagal");
+    }
+    ?>
+    <!-- Koneksi Database -->
+    <?php
+    include "../koneksi.php";
+    $select = "SELECT * FROM admin";
+    $hasil = mysqli_query($koneksi, $select);
+    $data = mysqli_fetch_array($hasil);
+    ?>
 <body>
     <header>
         <div class="container">
